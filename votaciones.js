32f52d8baab48031644a60410e0a733a8fce8c39
Saig Emiliano <emilianonicolas1995@gmail.com>
2022-03-10 18:32:23 -0300

    ultimo commit

---
+++ votaciones.js
@@ -0,0 +1,9 @@
+
+
+$('.toggle').on('click', function() {
+    $('.menu').toggleClass('expanded');  
+    $('span').toggleClass('hidden');  
+    $('.container , .toggle').toggleClass('close');  
+  });
+
+  
